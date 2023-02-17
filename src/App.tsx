3cca27e44b4a24df9bf126a1887aa321124fe2a7
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-02-17 00:49:06 -0800

    radix-ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
+import * as Slider from '@radix-ui/react-slider'
+import * as Switch from '@radix-ui/react-switch'
+import styles from './radix-styles.module.css'
+
 function App() {
   return (
-    <main>
-      <h1>Simple, traffic-based pricing</h1>
-      <p>Sign-up for our 30-day trial. No credit card required.</p>
+    <main className='flex min-h-screen flex-col items-center justify-center gap-16'>
+      <header className='flex flex-col items-center gap-2'>
+        <h1 className='text-xl font-semibold'>Simple, traffic-based pricing</h1>
+        <p className='text-center text-sm leading-6 text-_grayish-blue'>
+          Sign-up for our 30-day trial.
+          <br />
+          No credit card required.
+        </p>
+      </header>
 
-      <p>Pageviews</p>
-      <div>$ /month</div>
+      <form className='flex max-w-xs flex-col items-center gap-8 rounded-lg bg-white'>
+        <div>
+          <p>Pageviews</p>
 
-      <div>Monthly Billing</div>
-      <div>Yearly Billing 25% discount</div>
+          <Slider.Root
+            defaultValue={[50]}
+            max={100}
+            step={1}
+            aria-label='Price'
+            className={styles.SliderRoot}
+          >
+            <Slider.Track className={styles.SliderTrack}>
+              <Slider.Range className={styles.SliderRange} />
+            </Slider.Track>
+            <Slider.Thumb className={styles.SliderThumb} />
+          </Slider.Root>
+          <div>$ /month</div>
+        </div>
 
-      <p>Unlimited websites</p>
-      <p>100% data ownership</p>
-      <p>Email reports</p>
+        <div className='flex gap-4'>
+          <div>Monthly Billing</div>
+          <Switch.Root className={styles.SwitchRoot}>
+            <Switch.Thumb className={styles.SwitchThumb} />
+          </Switch.Root>
+          <div>
+            Yearly Billing <span>-25%</span>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <p>Unlimited websites</p>
+          <p>100% data ownership</p>
+          <p>Email reports</p>
+        </div>
 
-      <button>Start my trial</button>
+        <button>Start my trial</button>
+      </form>
     </main>
   )
 }
